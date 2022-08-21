297d27741633ed7651393497452df75ecd636b8a
Enmanuel Verdesia Suarez <everdesiasuarez@gmail.com>
2022-08-21 12:14:58 -0400

    feat: Add binding constraint to records

--- models/record.go
+++ models/record.go
@@ -1,42 +1,45 @@
 package models
 
 type Record struct {
-	Domain string `json:"domain" binding:"required"`
-	Ttl    string `json:"ttl"`
-	Class  string `json:"class"`
-	Type   string `json:"type"`
+	Id         uint64 `json:"id" gorm:"primaryKey"`
+	Ttl        string `json:"ttl" binding:"min=2"`
+	Class      string `json:"class" binding:"min=1"`
+	DomainName string `json:"domainName" binding:"min=1"`
+	Domain     Domain `gorm:"references:Name;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type SOARecord struct {
 	Record
-	NameServer string `json:"nameServer" binding:"required"`
-	Admin      string `json:"admin"`
+	NameServer string `json:"nameServer" binding:"min=1"`
+	Admin      string `json:"admin" binding:"min=1"`
 	Serial     uint   `json:"serial"`
-	Refresh    string `json:"refresh"`
-	Retry      string `json:"retry"`
-	Expire     string `json:"expire"`
-	Minimum    string `json:"minimum"`
-}
-
-type ARecord struct {
-	Record
-	Ip string `json:"ip" binding:"required"`
-}
-
-type AAAARecord struct {
-	Record
-	Ip string `json:"ip" binding:"required"`
+	Refresh    uint   `json:"refresh" binding:"gt=0"`
+	Retry      uint   `json:"retry" binding:"gt=0"`
+	Expire     uint   `json:"expire" binding:"gt=0"`
+	Minimum    uint   `json:"minimum" binding:"gt=0"`
 }
 
 type NSRecord struct {
 	Record
-	NameServer string `json:"nameServer" binding:"required"`
+	NameServer string `json:"nameServer" binding:"min=1"`
 }
+
+type ARecord struct {
+	Record
+	Name string `json:"name" binding:"min=1"`
+	Ip   string `json:"ip" binding:"ipv4"`
+}
+
+// type AAAARecord struct {
+// 	Record
+// 	Name string `json:"name" binding:"min=1"`
+// 	Ip   string `json:"ip" binding:"ipv6"`
+// }
 
 type MXRecord struct {
 	Record
-	Priority    int    `json:"priority" binding:"required"`
-	EmailServer string `json:"emailServer" binding:"required"`
+	Priority    uint   `json:"priority" binding:"gt=0"`
+	EmailServer string `json:"emailServer" binding:"min=1"`
 }
 
 // TODO: Add more types of records
